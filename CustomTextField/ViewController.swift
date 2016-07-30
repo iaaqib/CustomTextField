970ce630fb11bdc9ab86daa4b1f9f679c500459e
Aaqib Hussain <silent_smah@hotmail.com>
2016-07-30 04:19:57 -0700

    final commit
    Using MRTextfield to change place holder colour, and size.

---
+++ CustomTextField/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  CustomTextField
+//
+//  Created by Aaqib Hussain on 30/07/2016.
+//  Copyright © 2016 Kode Snippets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var customTextField: MRTextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //Place holder textsize
+//        customTextField.textSizePlaceHolder = 20.0
+//        customTextField.textColorPlaceHolder = UIColor.greenColor()
+//        /**Textholder underline could be 0 or 1
+//        */
+//        customTextField.style = 1
+//        //Changes the color of underline when typed
+//        customTextField.highlightLineColor = UIColor.greenColor()
+//        
+//        customTextField.iconImage = UIImage(named:"user")
+//        
+//        customTextField.dropDown = UIImage(named:"ok")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
